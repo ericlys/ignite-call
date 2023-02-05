a66ae60c73a11116809b3caa491d0dcce03f4fe0
Ericlys Moreira <ericlysm@gmail.com>
2023-02-04 22:03:00 -0300

    feat: calendar style

---
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -0,0 +1,10 @@
+import { Calendar } from '@/components/Calendar'
+import { Container } from './styled'
+
+export function CalendarStep() {
+  return (
+    <Container>
+      <Calendar />
+    </Container>
+  )
+}
